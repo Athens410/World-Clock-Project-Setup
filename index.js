d2466b44e4aa056d846562233072eac76eff6a07
Athens410 <athenah.coucean@gmail.com>
2023-07-15 13:47:20 -0400

    added timezonez for each city
    also added an id of cities to access from javascript

--- index.js
+++ index.js
@@ -39,3 +39,38 @@ tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss:SSS [<small>]A[</small>] 
  updateTime();
  setInterval(updateTime , 1000);
 
+
+
+
+ function updateCity(event){
+
+let cityTimeZone = event.target.value;
+
+let cityName =cityTimeZone.replace("_", "").split("/")[1];
+let cityTime = moment().tz(cityTimeZone);
+
+let citiesElement = document.querySelector("#cities");
+citiesElement.innerHTML = `
+
+
+<div class="city" >
+
+<h2>${cityName}</h2>
+<div class="date">
+    ${cityTime.format("MMMM Do YYYY")}
+</div>
+<div class="time">
+   ${cityTime.format("h:mm:ss:SSS")}<small>${cityTime.format("A")}</small>
+</div>
+
+</div>
+
+
+
+
+`;
+
+ }
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity)
